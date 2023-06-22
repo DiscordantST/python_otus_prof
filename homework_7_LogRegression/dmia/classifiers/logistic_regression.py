ed0f681657414d32de4b6f569ed8f95633bc9ef2
DiscordantST <discordantstas@gmail.com>
2023-06-22 22:35:19 +0300

    refactoring homework_7

--- homework_7_LogRegression/dmia/classifiers/logistic_regression.py
+++ homework_7_LogRegression/dmia/classifiers/logistic_regression.py
@@ -2,10 +2,6 @@ import numpy as np
 from scipy import sparse
 
 
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
-
-
 class LogisticRegression:
     def __init__(self):
         self.w = None
@@ -15,7 +11,6 @@ class LogisticRegression:
               batch_size=200, verbose=False):
         """
         Train this classifier using stochastic gradient descent.
-
         Inputs:
         - X: N x D array of training data. Each training point is a D-dimensional
              column.
@@ -25,8 +20,6 @@ class LogisticRegression:
         - num_iters: (integer) number of steps to take when optimizing
         - batch_size: (integer) number of training examples to use at each step.
         - verbose: (boolean) If true, print progress during optimization.
-
-
         Outputs:
         A list containing the value of the loss function at each training iteration.
         """
@@ -51,7 +44,6 @@ class LogisticRegression:
             # Hint: Use np.random.choice to generate indices. Sampling with         #
             # replacement is faster than sampling without replacement.              #
             #########################################################################
-
             indices = np.random.choice(num_train, batch_size)
             X_batch = X[indices, :]
             y_batch = y[indices]
@@ -63,6 +55,7 @@ class LogisticRegression:
             # evaluate loss and gradient
             loss, gradW = self.loss(X_batch, y_batch, reg)
             self.loss_history.append(loss)
+
             # perform parameter update
             #########################################################################
             # TODO:                                                                 #
@@ -75,7 +68,7 @@ class LogisticRegression:
             #########################################################################
 
             if verbose and it % 100 == 0:
-                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+                print ('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
         return self
 
@@ -83,11 +76,9 @@ class LogisticRegression:
         """
         Use the trained weights of this linear classifier to predict probabilities for
         data points.
-
         Inputs:
         - X: N x D array of data. Each row is a D-dimensional point.
         - append_bias: bool. Whether to append bias before predicting or not.
-
         Returns:
         - y_proba: Probabilities of classes for the data in X. y_pred is a 2-dimensional
           array with a shape (N, 2), and each row is a distribution of classes [prob_class_0, prob_class_1].
@@ -100,8 +91,8 @@ class LogisticRegression:
         # Hint: It might be helpful to use np.vstack and np.sum                   #
         ###########################################################################
 
-        predictions = sigmoid(X.dot(self.w.T))
-        y_proba = np.vstack([1 - predictions, predictions]).T
+        predictions = self.sigmoid(X.dot(self.w.T))
+        y_proba = np.vstack([1-predictions, predictions]).T
 
         ###########################################################################
         #                           END OF YOUR CODE                              #
@@ -111,20 +102,18 @@ class LogisticRegression:
     def predict(self, X):
         """
         Use the ```predict_proba``` method to predict labels for data points.
-
         Inputs:
         - X: N x D array of training data. Each column is a D-dimensional point.
-
         Returns:
         - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
           array of length N, and each element is an integer giving the predicted
           class.
         """
+
         ###########################################################################
         # TODO:                                                                   #
         # Implement this method. Store the predicted labels in y_pred.            #
         ###########################################################################
-
         y_proba = self.predict_proba(X, append_bias=True)
         y_pred = y_proba.argmax(axis=1)
 
@@ -132,6 +121,9 @@ class LogisticRegression:
         #                           END OF YOUR CODE                              #
         ###########################################################################
         return y_pred
+
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
 
     def loss(self, X_batch, y_batch, reg):
         """Logistic Regression loss function
@@ -145,34 +137,26 @@ class LogisticRegression:
         """
         dw = np.zeros_like(self.w)  # initialize the gradient as zero
         loss = 0
+
         # Compute loss and gradient. Your code should not contain python loops.
-
-        # Hypotesis
-        h = sigmoid(X_batch.dot(self.w))
-
-        # Gradient
-        dw = (h - y_batch) * X_batch
-
-        # Loss function
-        loss = -np.dot(y_batch, np.log(h)) - (1.0 - y_batch).dot(np.log(1.0 - h))
+        y_pred = self.sigmoid(X_batch.dot(self.w))
+        loss = -np.dot(y_batch, np.log(y_pred)) - np.dot((1-y_batch), np.log(1.0-y_pred))
+        dw = (y_pred- y_batch) * X_batch
 
         # Right now the loss is a sum over all training examples, but we want it
         # to be an average instead so we divide by num_train.
         # Note that the same thing must be done with gradient.
         num_train = X_batch.shape[0]
-
-        dw /= num_train
-        loss /= num_train
+        loss = loss / num_train
+        dw = dw / num_train
 
         # Add regularization to the loss and gradient.
         # Note that you have to exclude bias term in regularization.
-        dw[:-1] += (self.w[:-1] * reg) / num_train
+        loss += (reg / (2.0 * num_train)) * np.dot(self.w[:-1], self.w[:-1])
+        dw[:-1] = dw[:-1] + (reg * self.w[:-1]) / num_train
 
-        # L2 regularization
-        loss += (reg / (num_train * 2.0)) * self.w[:-1].dot(self.w[:-1])
         return loss, dw
 
     @staticmethod
     def append_biases(X):
         return sparse.hstack((X, np.ones(X.shape[0])[:, np.newaxis])).tocsr()
-
